96f8be2b4af139120043d3eebb257c5d92597187
sigiltenebrae <ck6202@rit.edu>
2022-07-28 11:56:33 -0400

    scrape themes for commander and final json output

--- scrapeedh.js
+++ scrapeedh.js
@@ -1,20 +1,31 @@
 const puppeteer = require('puppeteer');
 const url = 'https://edhrec.com/commanders/'
-const card = 'jon-irenicus-shattered-one';
 
-async function scrape() {
+async function scrape_commander(commander) {
+    let commander_data = {}
+    commander_data.commander = commander;
+
     const browser = await puppeteer.launch({});
     const page = await browser.newPage();
+    const card = commander.toLowerCase().replace(/[`~!@#$%^&*()_|+=?;:'",.<>\{\}\[\]\\\/]/gi, '').replaceAll(' ', '-');
     await page.goto(url + card);
 
-
+    let rec_scrape_theme = [];
+    const theme_list = await page.$('div.NavigationPanel_theme__3k48W');
+    const themes = await theme_list.$$('option');
+    for (let theme of themes) {
+        const temp_theme = await theme.evaluate(el => el.innerText.match(/([A-Z])\w+/g));
+        if (temp_theme) {
+            rec_scrape_theme.push(temp_theme.join(" "));
+        }
+    }
+    commander_data.themes = rec_scrape_theme;
 
     const categories = await page.$$('div.CardView_cardlist__1CcNe');
-    let rec_scrape = []
+    let rec_scrape_card = [];
     for (let cat of categories) {
-        //const inside = await cat.evaluate(el => el.textContent);
         let category_scrape = []
-        let category_name = await cat.$eval('h3', el => el.innerText);
+        let category_name = await cat.$eval('h3', el => el.innerText.match(/([A-Z])\w+/g).join(" "));
         let cards = await cat.$$('div.CardView_card__2vJOP');
         for (let card of cards) {
             let card_name = await card.$eval('div.Card_name__1MYwa', el => el.innerHTML);
@@ -29,15 +40,20 @@ async function scrape() {
                 }
             );
         }
-        rec_scrape.push(
+        rec_scrape_card.push(
             {
                 category: category_name,
                 cards: category_scrape
             }
         );
     }
-    console.log(JSON.stringify(rec_scrape));
+    commander_data.categories = rec_scrape_card;
+    //console.log(JSON.stringify(commander_data));
     await browser.close();
 }
 
-scrape();
+async function scrape_themes() {
+
+}
+
+//scrape_commander('Nath of the Gilt-Leaf');
